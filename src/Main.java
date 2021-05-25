6b7094c6a3b1e2555f04c444b7290b115d4c4cd5
Bryan <bryan.guzman@avantica.com>
2021-05-24 19:44:57 -0600

    Practica, clase del nombre del papa y la instancia en el main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import com.ucreativa.familia.Bryan;
+import com.ucreativa.familia.Omar;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Bryan br = new Bryan();
-
+        Omar padre = new Omar("Hola soy el papa de Bryan jaja");
     }
 }
---
+++ src/com/ucreativa/familia/Omar.java
@@ -0,0 +1,8 @@
+package com.ucreativa.familia;
+
+public class Omar {
+
+    public Omar(String saludo){
+        System.out.println("Este es el saludo: " + saludo);
+    }
+}
