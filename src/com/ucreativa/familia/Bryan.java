538ce641df88da91ae0bab5a40d2c343011e60bd
Bryan <bryan.guzman@avantica.com>
2021-05-27 19:37:29 -0600

    Utilizando herencia en las clase Bryan..

--- src/com/ucreativa/familia/Bryan.java
+++ src/com/ucreativa/familia/Bryan.java
@@ -1,8 +1,11 @@
 package com.ucreativa.familia;
 
-public class Bryan {
+public class Bryan extends Omar{
 
     public Bryan(){
+
+        super("saludo prueba"); //el saludo es el parametro del constructor Omar
+
         System.out.println("Buenas noches Bryan..!!");
     }
 }
